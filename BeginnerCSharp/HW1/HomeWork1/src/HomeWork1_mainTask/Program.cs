ad41c1165fcc97d0accbc01359143f5ea293c84e
Tarxos <ftarxos@gmail.com>
2021-01-16 00:50:50 +0300

    added a greeting to the program
    configure to get user name,
     make it first symbol ToUpper and other's ToLower,
     after trim name,
     and print to console with current date

--- BeginnerCSharp/HW1/HomeWork1/src/HomeWork1_mainTask/Program.cs
+++ BeginnerCSharp/HW1/HomeWork1/src/HomeWork1_mainTask/Program.cs
@@ -2,4 +2,15 @@
 using static System.Console;
 
 
-WriteLine("Hello World!");
+#region Program Body
+
+Write("Пожалуйста представьтесь: ");
+var userName = ReadLine();
+var name = (userName?[0].ToString().ToUpperInvariant() + userName?[1..].ToLowerInvariant()).Trim();
+
+WriteLine($"\r\nПривет, {(name is {Length: > 0} user ? user : "Noname")}, сегодня {DateTime.Now:dd.MM.yyyy}");
+
+#endregion
+
+// Program stop
+ReadLine();
