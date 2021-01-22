5e58392034dbec412efcd9022ae1b6a6006edf8a
Tarxos <ftarxos@gmail.com>
2021-01-23 00:41:13 +0300

    Task 1 solved

--- BeginnerCSharp/HW3/HomeWork3/Task1/Program.cs
+++ BeginnerCSharp/HW3/HomeWork3/Task1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using static System.Console;
 
 namespace Task1
 {
@@ -6,7 +6,19 @@ namespace Task1
     {
         static void Main(string[] args)
         {
+            const byte size = 5;
+            var array = new int[size, size];
+
+            for (var i = 0; i < size; i++)
+                for (var j = 0; j < size; j++)
+                    array[i, j] = i + j;
+
+            for (int i = 0,j = 0; i < size; i++, j++)
+                Write(array[i,j]);
             
+            // Program stop
+            WriteLine();
+            ReadLine();
         }
     }
 }
