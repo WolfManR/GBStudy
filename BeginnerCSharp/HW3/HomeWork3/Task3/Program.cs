f3e26271193277ea6bc8e83388c3929e05da8661
Tarxos <ftarxos@gmail.com>
2021-01-24 01:36:16 +0300

    Task 3 solved

--- BeginnerCSharp/HW3/HomeWork3/Task3/Program.cs
+++ BeginnerCSharp/HW3/HomeWork3/Task3/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using static System.Console;
 
 namespace Task3
 {
@@ -6,7 +8,13 @@ namespace Task3
     {
         static void Main(string[] args)
         {
+            WriteLine("Input something and i reverse it:");
+            var input = ReadLine().AsSpan();
+            var sb = new StringBuilder();
+            for (var i = input.Length - 1; i >= 0; i--)
+                sb.Append(input[i]);
             
+            WriteLine($"Reverced:\n{sb}");
         }
     }
 }
