72f994088fc8f6a47356fdc694adcea39bd34a99
Tarxos <ftarxos@gmail.com>
2021-01-26 23:49:58 +0300

    Task 1 Solved

--- BeginnerCSharp/HW4/HomeWork4/Task1/Program.cs
+++ BeginnerCSharp/HW4/HomeWork4/Task1/Program.cs
@@ -6,7 +6,21 @@ namespace Task1
     {
         static void Main(string[] args)
         {
+            (string firstName, string lastName, string patronimic)[] array = {
+                ("Elena","Sakurdina","Petrova"),
+                ("Petr","Kadrovich","Alexandovich"),
+                ("Vasiliy","Pupkin","Valilievich"),
+                ("Alexandra","Cyrilenko","Sergeevna")
+            };
+
+            for (var i = 0; i < array.Length; i++)
+                Console.WriteLine(GetFullName(array[i].firstName,array[i].lastName,array[i].patronimic));
             
+            // Program Stop
+            Console.ReadLine();
         }
+
+        static string GetFullName(string firstName, string lastName, string patronymic) => 
+            $"{firstName} {lastName} {patronymic}";
     }
 }
