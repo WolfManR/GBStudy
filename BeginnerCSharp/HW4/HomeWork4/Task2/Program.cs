71adc2f036e2d8417114e28e6097b33d522e8c5c
Tarxos <ftarxos@gmail.com>
2021-01-27 00:34:12 +0300

    Task 2 Solved

--- BeginnerCSharp/HW4/HomeWork4/Task2/Program.cs
+++ BeginnerCSharp/HW4/HomeWork4/Task2/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+using static System.Console;
 
 namespace Task2
 {
@@ -6,7 +8,34 @@ namespace Task2
     {
         static void Main(string[] args)
         {
+            WriteLine(@"Input numbers in one line, separating with ',', 
+if you input float number to separate float's number's from integer's use '.' instead of ','");
+            var input = ReadLine();
             
+            if (input is null)
+            {
+                WriteLine("Incorrect input");
+                return;
+            }
+            
+            var numberStrings = input.Split(',',StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+
+            decimal sum = 0;
+            for (var i = 0; i < numberStrings.Length; i++)
+            {
+                if (!decimal.TryParse(numberStrings[i], NumberStyles.AllowDecimalPoint,CultureInfo.InvariantCulture, out var number))
+                {
+                    WriteLine($"Incorrect input in numbers {numberStrings[i]}");
+                    return;
+                }
+
+                sum += number;
+            }
+            
+            WriteLine($"Sum of number's is  {sum:f4}");
+            
+            // Program Stop
+            ReadLine();
         }
     }
 }
