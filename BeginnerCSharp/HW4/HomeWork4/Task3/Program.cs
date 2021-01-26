4ff8d0d823c13bed3466c9e3166ed4c4cb0782ee
Tarxos <ftarxos@gmail.com>
2021-01-27 01:04:50 +0300

    Task 3 solved

--- BeginnerCSharp/HW4/HomeWork4/Task3/Program.cs
+++ BeginnerCSharp/HW4/HomeWork4/Task3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace Task3
 {
@@ -6,7 +7,35 @@ namespace Task3
     {
         static void Main(string[] args)
         {
+            WriteLine("input your current month number");
+            var input = ReadLine().AsSpan();
+
+            if (!int.TryParse(input, out var monthNumber))
+            {
+                WriteLine("Incorrect input");
+                return;
+            }
+
+            if (monthNumber < 1 || monthNumber > 12)
+            {
+                WriteLine("Incorrect input, needed number must be in [1..12]");
+                return;
+            }
+
+            var season = monthNumber switch
+                         {
+                             12 or <= 2 => YearSeason.Winter,
+                             <=5        => YearSeason.Spring,
+                             <=7        => YearSeason.Summer,
+                             _          => YearSeason.Autumn
+                         };
+            WriteLine($"Current season is {season}");
             
+        }
+
+        private enum YearSeason
+        {
+            Winter,Spring,Summer,Autumn
         }
     }
 }
