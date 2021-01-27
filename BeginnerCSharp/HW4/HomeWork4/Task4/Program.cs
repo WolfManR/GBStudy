e055b09395397bd1273a9bd4a079dc586b7b525f
Tarxos <ftarxos@gmail.com>
2021-01-27 23:07:50 +0300

    Task 4 simple solution done

--- BeginnerCSharp/HW4/HomeWork4/Task4/Program.cs
+++ BeginnerCSharp/HW4/HomeWork4/Task4/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
-
+using static System.Console;
 namespace Task4
 {
     class Program
     {
         static void Main(string[] args)
         {
+            WriteLine("Input a number for calculating it's fibonacci");
+            var input = ReadLine().AsSpan();
+
+            if (!int.TryParse(input, out var n))
+            {
+                WriteLine("incorrect number");
+                return;
+            }
             
+            WriteLine($"Fibonacci of {n} is {Fibonacci(n)}");
+            
+            // Program Stop
+            ReadLine();
         }
+
+        private static int Fibonacci(int n) =>
+            n switch
+            {
+                0 => 0,
+                1 => 1,
+                _ => Fibonacci(n - 1) + Fibonacci(n - 2)
+            };
     }
 }
