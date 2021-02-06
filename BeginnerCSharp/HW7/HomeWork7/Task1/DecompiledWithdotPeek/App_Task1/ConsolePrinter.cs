285e0a7d9a8be3a36e9ec83af4dbea24b44692b0
Tarxos <ftarxos@gmail.com>
2021-02-07 02:31:29 +0300

    Decompile net 5 project with dotPeek for solving Task 1, and fix it's for build removing AssemblyInfo file, and clear csproj file from MSBuild junk

---
+++ BeginnerCSharp/HW7/HomeWork7/Task1/DecompiledWithdotPeek/App_Task1/ConsolePrinter.cs
@@ -0,0 +1,40 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: AppFor_Task1_Task2.ConsolePrinter
+// Assembly: AppFor_Task1_Task2, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: 7C441126-F631-4792-8FD5-C6E9D2B261ED
+// Assembly location: D:\Projects\Programming\CSharp\Studi\GeekBrains\GBStudy\BeginnerCSharp\HW7\HomeWork7\AppFor_Task1_Task2\bin\Debug\net5.0\AppFor_Task1_Task2.dll
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AppFor_Task1_Task2
+{
+  public class ConsolePrinter
+  {
+    private readonly List<CharConsoleColor> _colors;
+    private const ConsoleColor DefaultColor = ConsoleColor.White;
+
+    public ConsolePrinter(IEnumerable<CharConsoleColor> colors) => this._colors = colors.ToList<CharConsoleColor>();
+
+    public void Print(string toPrint)
+    {
+      int length = toPrint.Length;
+      for (int i = 0; i < length; i++)
+      {
+        CharConsoleColor charConsoleColor = this._colors.Find((Predicate<CharConsoleColor>) (c => (int) (char) c == (int) toPrint[i]));
+        if (charConsoleColor == null)
+          Console.Write(toPrint[i]);
+        else
+          ConsolePrinter.PrintWithColor(toPrint[i], (ConsoleColor) charConsoleColor);
+      }
+    }
+
+    private static void PrintWithColor(char character, ConsoleColor color)
+    {
+      Console.ForegroundColor = color;
+      Console.Write(character);
+      Console.ForegroundColor = ConsoleColor.White;
+    }
+  }
+}
---
+++ BeginnerCSharp/HW7/HomeWork7/Task1/DecompiledWithdotPeek/App_Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: AppFor_Task1_Task2.Program
+// Assembly: AppFor_Task1_Task2, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: 7C441126-F631-4792-8FD5-C6E9D2B261ED
+// Assembly location: D:\Projects\Programming\CSharp\Studi\GeekBrains\GBStudy\BeginnerCSharp\HW7\HomeWork7\AppFor_Task1_Task2\bin\Debug\net5.0\AppFor_Task1_Task2.dll
+
+using System;
+using System.Collections.Generic;
+
+namespace AppFor_Task1_Task2
+{
+  internal class Program
+  {
+    private static void Main(string[] args) => new ConsolePrinter((IEnumerable<CharConsoleColor>) new CharConsoleColor[4]
+    {
+      new CharConsoleColor('H', ConsoleColor.Cyan),
+      new CharConsoleColor('l', ConsoleColor.Yellow),
+      new CharConsoleColor('W', ConsoleColor.Red),
+      new CharConsoleColor('d', ConsoleColor.Magenta)
+    }).Print("Hello World!");
+  }
+}
