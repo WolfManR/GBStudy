a21f6530be5806cfe1918c6ab1bddd5dbc8b7c33
Tarxos <ftarxos@gmail.com>
2021-02-25 23:21:43 +0300

    benchmark

--- Algoritms/HomeWork3/DistanceBenchmark/Program.cs
+++ Algoritms/HomeWork3/DistanceBenchmark/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using BenchmarkDotNet.Running;
 
 namespace DistanceBenchmark
 {
@@ -6,7 +6,21 @@ namespace DistanceBenchmark
     {
         static void Main(string[] args)
         {
-            
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
         }
+        
+        // TestCount = 100_000
+        //
+        // |                   Method |      Mean |    Error |    StdDev |
+        // |------------------------- |----------:|---------:|----------:|
+        // |              Float_Class | 131.25 us | 5.659 us | 15.775 us |
+        // |             Float_Struct |  84.26 us | 1.424 us |  1.332 us |
+        // |            Double_Struct | 110.22 us | 2.186 us |  4.367 us |
+        // | Float_Struct_WithoutSQRT |  84.57 us | 1.396 us |  1.306 us |
+        //
+        // * Warnings *
+        // MultimodalDistribution
+        //     BenchmarkClass.Float_Class: Default -> It seems that the distribution is bimodal (mValue = 3.7)
+
     }
 }
