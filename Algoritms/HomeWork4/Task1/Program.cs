70e22c88e5abf1ae6e848653fb12e6b29ca8ee3f
Tarxos <ftarxos@gmail.com>
2021-03-01 22:14:25 +0300

    Blank for testing search strings

--- Algoritms/HomeWork4/Task1/Program.cs
+++ Algoritms/HomeWork4/Task1/Program.cs
@@ -1,10 +1,37 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
 
 namespace Task1
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<BenchmarkClass>();
+        }
+    }
+    
+    public class BenchmarkClass
+    {
+        private const int Words = 9;
+        private const int Sentences = 1;
+        private const int Strings = 10000;
+        
+        private static string[] _array = LoremNET.Lorem.Paragraphs(Words, Sentences, Strings).ToArray();
+        private static HashSet<string> _hashSet = LoremNET.Lorem.Paragraphs(Words, Sentences, Strings).ToHashSet();
+
+        
+        
+        [Benchmark]
+        public void Search_In_Array()
+        {
+            
+        }
+        
+        [Benchmark]
+        public void Search_In_HashSet()
         {
             
         }
