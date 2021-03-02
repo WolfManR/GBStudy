9a7f2724a6478e4823f52a3f38158c42d39c2913
Tarxos <ftarxos@gmail.com>
2021-03-03 01:53:15 +0300

    Task 1 solved

--- Algoritms/HomeWork4/Task1/Program.cs
+++ Algoritms/HomeWork4/Task1/Program.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNet.Running;
 
 namespace Task1
 {
@@ -11,29 +8,19 @@ namespace Task1
         {
             BenchmarkRunner.Run<BenchmarkClass>();
         }
-    }
-    
-    public class BenchmarkClass
-    {
-        private const int Words = 9;
-        private const int Sentences = 1;
-        private const int Strings = 10000;
-        
-        private static string[] _array = LoremNET.Lorem.Paragraphs(Words, Sentences, Strings).ToArray();
-        private static HashSet<string> _hashSet = LoremNET.Lorem.Paragraphs(Words, Sentences, Strings).ToHashSet();
 
-        
-        
-        [Benchmark]
-        public void Search_In_Array()
-        {
-            
-        }
-        
-        [Benchmark]
-        public void Search_In_HashSet()
-        {
-            
-        }
+        //  Results of test
+        //
+        //  |                           Method |         Mean |      Error |       StdDev |
+        //  |--------------------------------- |-------------:|-----------:|-------------:|
+        //  |                Search_In_HashSet |     32.70 ns |   0.615 ns |     0.545 ns |
+        //  |     Search_In_Array_With_Foreach | 40,079.69 ns | 753.428 ns |   806.160 ns |
+        //  | Search_In_Array_With_Reverse_For | 45,594.94 ns | 879.993 ns |   823.146 ns |
+        //  |                  Search_In_Array | 46,408.10 ns | 879.832 ns |   822.996 ns |
+        //  |        Search_In_Array_With_LINQ | 49,904.22 ns | 978.457 ns | 1,861.615 ns |
+        //
+        //  * Warnings *
+        //  MultimodalDistribution
+        //      BenchmarkClass.Search_In_Array_With_LINQ: Default -> It seems that the distribution is bimodal (mValue = 3.75)
     }
 }
