f5e979e028cb5f4b55dfd91137cc404d21d92305
Tarxos <ftarxos@gmail.com>
2021-03-14 22:34:09 +0300

    struct for Lesson 8 solution

---
+++ Algoritms/HomeWork8/SortHomeWork/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SortHomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    public class Sorts
+    {
+        public void Bucket()
+        {
+            
+        }
+        
+        public void External()
+        {
+            
+        }
+    }
+}
