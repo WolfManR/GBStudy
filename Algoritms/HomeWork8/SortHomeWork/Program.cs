f520a9b97a1fedd7dfad77f30a5fbc0be9e302eb
Tarxos <ftarxos@gmail.com>
2021-03-17 20:58:14 +0300

    Bucket sort

--- Algoritms/HomeWork8/SortHomeWork/Program.cs
+++ Algoritms/HomeWork8/SortHomeWork/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SortHomeWork
 {
@@ -6,20 +7,93 @@ namespace SortHomeWork
     {
         static void Main(string[] args)
         {
-            
+            var arr = new int[14];
+            arr.Fill(2, 30).Print().Bucket(4).Print();
         }
     }
 
-    public class Sorts
+    public static class Sorts
     {
-        public void Bucket()
+        public static int[] Bucket(this int[] self, int numberOfBuckets)
         {
-            
+            var (min, max) = self.GetMinAndMaxOfArray();
+            var bucketSize = (max - min) / numberOfBuckets;
+            List<List<int>> buckets = new();
+
+            for (var i = 0; i < numberOfBuckets; i++)
+                buckets.Add(new());
+
+            self = MergeBuckets(
+                        SortBuckets(
+                            SeparateOnBuckets(self, min, bucketSize, buckets)),
+                        self.Length);
+
+            return self;
+
+            // Actual methods can be pulled out to Extension method's with support of Fluent Call
+            static IReadOnlyList<List<int>> SeparateOnBuckets(int[] array, int minValue, int bucketSize, IReadOnlyList<List<int>> buckets)
+            {
+                for (var i = 0; i < array.Length; i++)
+                {
+                    var bucketIndex = (array[i] - minValue) / bucketSize;
+                    if (array[i] != minValue && bucketIndex > 0)
+                        buckets[bucketIndex - 1].Add(array[i]);
+                    else
+                        buckets[bucketIndex].Add(array[i]);
+                }
+
+                return buckets;
+            }
+
+            static IReadOnlyList<List<int>> SortBuckets(IReadOnlyList<List<int>> buckets)
+            {
+                for (var i = 0; i < buckets.Count; i++)
+                {
+                    if (buckets.Count > 0)
+                        buckets[i].Sort();
+                }
+
+                return buckets;
+            }
+
+            static int[] MergeBuckets(IReadOnlyList<List<int>> buckets, int originalArraySize)
+            {
+                var resultArray = new int[originalArraySize];
+                var k = 0;
+                for (var i = 0; i < buckets.Count; i++)
+                {
+                    for (var j = 0; j < buckets[i].Count; j++)
+                    {
+                        resultArray[k++] = buckets[i][j];
+                    }
+                }
+
+                return resultArray;
+            }
         }
-        
-        public void External()
+
+
+        public static int[] External(this int[] self)
         {
             
+            
+            return self;
+        }
+
+
+        private static (int min, int max) GetMinAndMaxOfArray(this int[] self)
+        {
+            var max = self[0];
+            var min = self[0];
+            for (var i = 1; i < self.Length; i++)
+            {
+                if (self[i] > max)
+                    max = self[i];
+                if (self[i] < min)
+                    min = self[i];
+            }
+
+            return (min, max);
         }
     }
 }
