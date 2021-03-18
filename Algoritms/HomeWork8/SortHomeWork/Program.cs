97ea86303d9a6d9a8bd3d001806a17465d3bed31
Tarxos <ftarxos@gmail.com>
2021-03-18 23:11:39 +0300

    External Sort

--- Algoritms/HomeWork8/SortHomeWork/Program.cs
+++ Algoritms/HomeWork8/SortHomeWork/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Text;
 
 namespace SortHomeWork
 {
@@ -10,7 +9,42 @@ namespace SortHomeWork
         static void Main(string[] args)
         {
             var arr = new int[14];
-            arr.Fill(2, 30).Print().Bucket(4).Print();
+            arr.Fill(2, 30).Print();
+            
+            // prepare for External Sort
+            const string outputDirectory = "ExternalSortTest";
+            var fileToSort = $"{outputDirectory}/toSort.txt";
+            if (!Directory.Exists(outputDirectory)) Directory.CreateDirectory(outputDirectory);
+            using (var sw = new StreamWriter(File.Create(fileToSort)))
+            {
+                for (var i = 0; i < arr.Length; i++)
+                {
+                    sw.WriteLine(arr[i]);
+                }
+            }
+            
+            // // Bucket Sort
+            // arr.Bucket(4).Print();
+            
+            // External Sort
+            var outputFilePath = Path.Combine(Path.GetDirectoryName(fileToSort), "output.txt");
+            try
+            {
+                Sorts.External(fileToSort, outputFilePath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return;
+            }
+
+            using (var sr = new StreamReader(File.OpenRead(outputFilePath)))
+            {
+                while (!sr.EndOfStream)
+                {
+                    Console.Write(sr.ReadLine() + " ");
+                }
+            }
         }
     }
 
@@ -26,14 +60,15 @@ namespace SortHomeWork
                 buckets.Add(new());
 
             self = MergeBuckets(
-                        SortBuckets(
-                            SeparateOnBuckets(self, min, bucketSize, buckets)),
-                        self.Length);
+                SortBuckets(
+                    SeparateOnBuckets(self, min, bucketSize, buckets)),
+                self.Length);
 
             return self;
 
             // Actual methods can be pulled out to Extension method's with support of Fluent Call
-            static IReadOnlyList<List<int>> SeparateOnBuckets(int[] array, int minValue, int bucketSize, IReadOnlyList<List<int>> buckets)
+            static IReadOnlyList<List<int>> SeparateOnBuckets(int[] array, int minValue, int bucketSize,
+                IReadOnlyList<List<int>> buckets)
             {
                 for (var i = 0; i < array.Length; i++)
                 {
@@ -74,94 +109,156 @@ namespace SortHomeWork
             }
         }
 
-
-        public static int[] External(this int[] self)
+        public static void External(string filePath, string outputFilePath)
         {
-            
-            
-            return self;
+            ExternalSorter sorter = new(5);
+            sorter.Sort(filePath, outputFilePath);
         }
 
-
-        private static (int min, int max) GetMinAndMaxOfArray(this int[] self)
+        
+        
+        
+        private class ExternalSorter
         {
-            var max = self[0];
-            var min = self[0];
-            for (var i = 1; i < self.Length; i++)
+            private readonly int _bucketSize;
+            private readonly List<string> _pathsToTempFiles = new();
+            private int _numberOfBuckets;
+            private static readonly string TempFileFormatter = TempDirectory + @"/temp{0}.txt";
+            private const string TempDirectory = "Temp";
+
+            public ExternalSorter(int bucketSize) => _bucketSize = bucketSize;
+
+
+            public void Sort(string fileToSort, string outputFilePath)
             {
-                if (self[i] > max)
-                    max = self[i];
-                if (self[i] < min)
-                    min = self[i];
+                ReadFile(fileToSort);
+                MergeBuckets(outputFilePath);
+                CleanAfterWork();
             }
 
-            return (min, max);
-        }
-    }
-
-    public class ExternalSorter
-    {
-        private string fileToRead;
-        private int originalFileSize;
-        private long bucketSize;
-        private List<string> pathsToTempFiles = new ();
-        void ReadFile()
-        {
-            var fs = new FileStream(fileToRead, FileMode.Open);
-            var sr = new StreamReader(fs);
-            var length = fs.Length;
-            bucketSize = length / 5;
-
-            if (!Directory.Exists("Temp")) 
-                Directory.CreateDirectory("Temp");
-
-            const string tempFileFormatter = @"Temp/temp{0}.txt";
-            var currentBucket = 0;
-            while (!sr.EndOfStream)
+            
+            
+            private void ReadFile(string fileToRead)
             {
-               var bucket =  FillBucket(sr, bucketSize, currentBucket == 4);
-                
-               bucket.Sort();
+                using var fs = new FileStream(fileToRead, FileMode.Open);
+                using var sr = new StreamReader(fs);
 
-               var bucketPath = string.Format(tempFileFormatter, currentBucket++);
-               pathsToTempFiles.Add(bucketPath);
-               SaveBucketToFile(bucketPath,bucket);
-            }
+                if (!Directory.Exists(TempDirectory))
+                    Directory.CreateDirectory(TempDirectory);
 
 
-            static List<int> FillBucket(StreamReader stream, long bucketSize, bool isLastBucket)
-            {
-                List<int> bucket = new();
-                var i = 0;
-                do
+                var currentBucketIndex = 0;
+
+                while (!sr.EndOfStream)
                 {
-                    var line = stream.ReadLine();
-                    if (!int.TryParse(line, out var number)) continue;
-                    
-                    bucket.Add(number);
-                    if(!isLastBucket)
+                    var bucket = FillBucket(sr, _bucketSize);
+                    _numberOfBuckets++;
+                    bucket.Sort();
+
+                    var bucketPath = string.Format(TempFileFormatter, currentBucketIndex++);
+                    _pathsToTempFiles.Add(bucketPath);
+                    SaveBucketToFile(bucketPath, bucket);
+                }
+
+
+                static List<int> FillBucket(StreamReader stream, long bucketSize)
+                {
+                    List<int> bucket = new();
+                    var i = 0;
+                    while (i < bucketSize && !stream.EndOfStream)
+                    {
+                        var line = stream.ReadLine();
+                        if (!int.TryParse(line, out var number)) continue;
+
+                        bucket.Add(number);
                         i++;
-                } while (i < bucketSize || !stream.EndOfStream);
+                    } 
+                    
+                    return bucket;
+                }
 
-                return bucket;
-            }
-
-            static void SaveBucketToFile(string filePath, List<int> bucket)
-            {
-                using var sw = new StreamWriter(File.Create(filePath));
-                for (var i = 0; i < bucket.Count; i++)
+                static void SaveBucketToFile(string filePath, List<int> bucket)
                 {
-                    sw.WriteLine(bucket[i]);
+                    using var sw = new StreamWriter(File.Create(filePath));
+                    for (var i = 0; i < bucket.Count; i++)
+                    {
+                        sw.WriteLine(bucket[i]);
+                    }
                 }
             }
-        }
 
-        void MergeBuckets(string outputPath)
-        {
-            if (pathsToTempFiles.Count <= 0)
-                throw new InvalidOperationException("There no one bucket");
-            
-            
+            private void MergeBuckets(string outputPath)
+            {
+                if (_pathsToTempFiles.Count <= 0)
+                    throw new InvalidOperationException("There no one bucket");
+
+                var numbersFromBuckets = new int?[_numberOfBuckets];
+                var buckets = new StreamReader[_numberOfBuckets];
+                for (var i = 0; i < _numberOfBuckets; i++)
+                {
+                    var stream = new StreamReader(File.OpenRead(_pathsToTempFiles[i]));
+                    buckets[i] = stream;
+                    numbersFromBuckets[i] = GetNumber(stream);
+                }
+
+                
+                
+                using var sw = new StreamWriter(File.Create(outputPath));
+                while (true)
+                {
+                    var (min, minBucketIndex) = GetMin(numbersFromBuckets);
+                    if (min is null) break;
+                    sw.WriteLine((int) min);
+                    numbersFromBuckets[minBucketIndex] = GetNumber(buckets[minBucketIndex]);
+                }
+
+                for (var i = 0; i < buckets.Length; i++)
+                {
+                    buckets[i].Dispose();
+                }
+                
+                
+
+                static int? GetNumber(StreamReader stream)
+                {
+                    if (stream.EndOfStream) return null;
+
+                    var line = stream.ReadLine();
+                    if (int.TryParse(line, out var firstNumber))
+                        return firstNumber;
+
+                    throw new("Not a number Exception");
+                }
+
+                static (int? min, int minBucketIndex) GetMin(int?[] numbers)
+                {
+                    var min = numbers[0];
+                    var minBucketIndex = 0;
+                    for (var i = 1; i < numbers.Length; i++)
+                    {
+                        if (min is not null && numbers[i] >= min) continue;
+                        min = numbers[i];
+                        minBucketIndex = i;
+                    }
+
+                    return (min, minBucketIndex);
+                }
+            }
+
+            private void CleanAfterWork()
+            {
+                var paths = _pathsToTempFiles;
+                for (var i = 0; i < paths.Count; i++)
+                {
+                    if(File.Exists(paths[i]))
+                        File.Delete(paths[i]);
+                }
+
+                if (!Directory.Exists(TempDirectory)) return;
+                var isNotClean = Directory.GetFileSystemEntries(TempDirectory).Length > 0;
+                if(isNotClean) return;
+                Directory.Delete(TempDirectory);
+            }
         }
     }
 }
